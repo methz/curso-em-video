b63b5b0d9ec5277fecd6292039f41de23fd04904
Marcos Oliveira <email.marcos.oliveira@gmail.com>
2020-03-08 15:56:43 -0300

    Initial commit

---
+++ python3/desafios/Desafio 01.py
@@ -0,0 +1,2 @@
+nome = input('Digite seu nome: ')
+print('Olá, humano! Então você se chama', nome,'? Seja muito bem vindo ao nosso sistema')
---
+++ python3/desafios/Desafio 02.py
@@ -0,0 +1,5 @@
+dia = input('Digite o dia de seu nascimento ')
+mes = input('Digite o mês de seu nascimento (Ex: Mar para Março)')
+ano = input('Digite o ano de seu nascimento (Ex: 1995) ')
+
+print('Você nasceu no dia', dia,'de',mes,'de',ano,'. Correto?')
